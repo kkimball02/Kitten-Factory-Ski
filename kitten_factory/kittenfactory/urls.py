5f4e588956ab9e65b806a3dcd24ca6033434854c
kkimball02 <156021805+kkimball02@users.noreply.github.com>
2024-04-09 13:44:03 -0600

    Finished Product Serializer and API View

--- kitten_factory/kittenfactory/urls.py
+++ kitten_factory/kittenfactory/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Login,Home
+from .views import Login, Home, ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
+from . import views
 
 urlpatterns = [
     path('login/', Login.as_view(), name ='login.html'),
     path('home/', Home.as_view(), name='home.html'),
+    path('product/', ProductListCreateAPIView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
 ]
--- kitten_factory/kittenfactory/views.py
+++ kitten_factory/kittenfactory/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from rest_framework import generics
-from django.urls import reverse
-from django import forms
-
+from django.urls import reverse, reverse_lazy
+from .models import Product
+from .serializers import ProductSerializer
 class Login(View):
 
     template_name ='login.html'
@@ -19,3 +19,14 @@ class Home(View):
     def get(self, request):
 
         return render(request, self.template_name,)
+    
+
+# List and Create
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Retrieve, Update and Delete
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
