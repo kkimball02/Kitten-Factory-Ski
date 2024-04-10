3b64a8920a2761cfb83f3f612595d0a4012c8e93
Your Name <you@example.com>
2024-04-09 20:16:55 -0600

    Added some things to admin, serializer, and views (still having issues)

--- kitten_factory/kittenfactory/serializers.py
+++ kitten_factory/kittenfactory/serializers.py
@@ -1,12 +1,84 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Product
+from .models import Customer
+from .models import Employee
+from .models import RawMaterial
+from .models import Order
+from .models import Return
+from .models import Inventory
+from .models import Supplier
+from .models import CustomerReview
+from .models import Payment
+from .models import SalesReport
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
-        
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
 
 
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+
+class RawMaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RawMaterial
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+
+class ReturnSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Return
+        fields = '__all__'
+
+
+class InventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventory
+        fields = '__all__'
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+
+class CustomerReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerReview
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = '__all__'
+
+class Meta:
+    model = SalesReport
+    fields = '__all__'
+
+class SalesReportSerializer(serializers.ModelSerializer):
+    most_popular_product = serializers.SerializerMethodField()
+    least_popular_product = serializers.SerializerMethodField()
+
+def get_most_popular_product(self, obj):
+    return obj.most_popular_product().name if obj.most_popular_product() else None
+def get_least_popular_product(self, obj):
+    return obj.least_popular_product().name if obj.least_popular_product() else None
--- kitten_factory/kittenfactory/views.py
+++ kitten_factory/kittenfactory/views.py
@@ -1,32 +1,66 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 from rest_framework import generics
-from django.urls import reverse, reverse_lazy
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Employee, Customer, Order, Return, RawMaterial
+from .serializers import ProductSerializer, EmployeeSerializer, CustomerSerializer, OrderSerializer, ReturnSerializer, RawMaterialSerializer
+
 class Login(View):
-
-    template_name ='login.html'
-
-    def get(self, request):
-
-        return render(request, self.template_name,)
+    template_name = 'login.html'
     
+    def get(self, request):
+        return render(request, self.template_name)
+
 class Home(View):
-
-    template_name ='home.html'
-
-    def get(self, request):
-
-        return render(request, self.template_name,)
+    template_name = 'home.html'
     
+    def get(self, request):
+        
+        return render(request, self.template_name,)
 
-# List and Create
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-# Retrieve, Update and Delete
+    
 class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class EmployeeListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class EmployeeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class CustomerListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class OrderListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class ReturnListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Return.objects.all()
+    serializer_class = ReturnSerializer
+
+class ReturnRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Return.objects.all()
+    serializer_class = ReturnSerializer
+
+class RawMaterialListCreateAPIView(generics.ListCreateAPIView):
+    queryset = RawMaterial.objects.all()
+    serializer_class = RawMaterialSerializer
+
+class RawMaterialRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = RawMaterial.objects.all()
+    serializer_class = RawMaterialSerializer
