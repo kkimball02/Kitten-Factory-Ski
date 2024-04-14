fd6682ee3e1e133af8e867e9e69864a19d95f0df
Easton <eastoncoleman20@yahoo.com>
2024-04-13 18:21:54 -0600

    Pull

--- kitten_factory/kittenfactory/forms.py
+++ kitten_factory/kittenfactory/forms.py
@@ -1,11 +1,18 @@
 # forms.py
 
 from django import forms
-from .models import CustomerReturn
+from .models import CustomerReturn, Order, Product
 
 class CustomerReturnForm(forms.ModelForm):
     class Meta:
         model = CustomerReturn
         fields = ['customer_name', 'product_name', 'reason']
 
-        
+class OrderForm(forms.ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.all(), required=True)
+    quantity = forms.IntegerField(min_value=1, required=True)
+
+    class Meta:
+        model = Order
+        fields = ['product', 'quantity']
+
--- kitten_factory/kittenfactory/urls.py
+++ kitten_factory/kittenfactory/urls.py
@@ -3,16 +3,16 @@ from . import views
 from .views import Home, ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView, EmployeeListCreateAPIView, Home
 from .views import EmployeeRetrieveUpdateDestroyAPIView, CustomerListCreateAPIView, CustomerRetrieveUpdateDestroyAPIView 
 from .views import OrderListCreateAPIView, OrderRetrieveUpdateDestroyAPIView, ReturnListCreateAPIView, ReturnRetrieveUpdateDestroyAPIView
-from .views import RawMaterialListCreateAPIView, RawMaterialRetrieveUpdateDestroyAPIView
+from .views import RawMaterialListCreateAPIView, RawMaterialRetrieveUpdateDestroyAPIView, product_list, delete_order
 from .views import SalesReportListCreateAPIView, SalesReportRetrieveUpdateDestroyAPIView, submit_customer_return
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('api/product', ProductListCreateAPIView.as_view(), name='product'),
-    path('api/product/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+    path('api/products/', ProductListCreateAPIView.as_view(), name='product'),
+    path('api/product/<int:pk>', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
     path('api/employee/', EmployeeListCreateAPIView.as_view(), name='employee-list'),
-    path('api/employee/<int:pk>/', EmployeeRetrieveUpdateDestroyAPIView.as_view(), name='employee-detail'),
+    path('api/employee/<int:pk>', EmployeeRetrieveUpdateDestroyAPIView.as_view(), name='employee-detail'),
     path('api/customer/', CustomerListCreateAPIView.as_view(), name='customer-list'),
     path('api/customer/<int:pk>', CustomerRetrieveUpdateDestroyAPIView.as_view(), name='customer-detail'),
     path('api/order/', OrderListCreateAPIView.as_view(), name='order-list'),
@@ -25,10 +25,14 @@ urlpatterns = [
     path('api/report/<int:pk>', SalesReportRetrieveUpdateDestroyAPIView.as_view(), name='report-details'),
     path('about/', views.Home.about_view, name='about'),
     path('contact/', views.Home.contact_view, name='contact'),
-    path('products/', views.Product_List.as_view(), name='product-list'),
     path('contact/submit/', Home.contact_submit, name='contact-submit'),
     path('submit_return/', submit_customer_return, name='submit-return'),
-    path('order_history/', views.order_history_view, name='order-history')
+    path('order_history/', views.order_history_view, name='order_history'),
+    path('order-form/', views.order_product_view, name='order-form'),
+    path('products/', product_list, name='product-list'),
+    path('order/<int:order_id>/delete', delete_order, name='delete-order'),
+    
+    
 ]
     
 
